d4fb95bd813bb2580955dc589f4cd33d9fbec30e
João Vitor Bonadiman <j.bonadiman@live.com>
2020-03-13 18:33:55 -0300

    adicionado middleware que conta requests

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,13 @@ const routes = require('./routes');
 
 const app = express();
 
+let requestCount = 0;
+
+function countRequest(req, res, next) {
+  console.log(`Number of requests: ${++requestCount}`);
+  next();
+};
+
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect('mongodb://root:admin@cluster0-shard-00-00-23um3.gcp.mongodb.net:27017,cluster0-shard-00-01-23um3.gcp.mongodb.net:27017,cluster0-shard-00-02-23um3.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
@@ -12,6 +19,7 @@ mongoose.connect('mongodb://root:admin@cluster0-shard-00-00-23um3.gcp.mongodb.ne
   useUnifiedTopology: true,
 });
 
+app.use(countRequest);
 app.use(express.json());
 app.use(routes);
 
